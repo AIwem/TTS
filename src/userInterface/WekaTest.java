b73b33e99c32f57d4ac7409869505665785e8820
hasheminamin <m.hashemi@gmail.com>
2017-03-05 14:47:13 +0330

    testing weka with and without crossValidation
    generating detail acuuracy reports

--- src/userInterface/WekaTest.java
+++ src/userInterface/WekaTest.java
@@ -41,14 +41,21 @@ public class WekaTest {
  
 	public static double calculateAccuracy(ArrayList<Prediction> predictions) {
 		double correct = 0;
- 
+		
+//		ArrayList<Prediction> wrongClassified = new ArrayList<Prediction>();
+		
 		for (int i = 0; i < predictions.size(); i++) {
 			NominalPrediction np = (NominalPrediction) predictions.get(i);
 			if (np.predicted() == np.actual()) {
 				correct++;
 			}
+//			else{
+//				wrongClassified.add(np);
+//			}
 		}
- 
+//		for(Prediction wr:wrongClassified)
+//			System.out.println(">>> wrongly classified " + wr.actual() + " as " + wr.predicted());
+		
 		return 100 * correct / predictions.size();
 	}
  
@@ -63,39 +70,8 @@ public class WekaTest {
 		return split;
 	}
  
-	public static void main(String[] args) throws Exception {		
+public static void withoutCrossValidation(Instances trainData, Instances testData) throws Exception {		
 		
-//		BufferedReader datafile = readDataFile("dataset/95-10-06DatasetStaticObj.arff");//stroy1
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObj.arff");//story 1&2
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObj.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObjState.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRole.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleAction.arff");//story1&2&3
-		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleState.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleIntent.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObj.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObjAction.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObjState.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetLocation.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetTime.arff");//story1&2&3
-//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetSceneGoal.arff");//story1&2&3
-		
-		BufferedReader dataTestfile = readDataFile("dataset/95-10-13DateSetRoleState-test.arff");//story1&2&3
- 
-		Instances data = new Instances(datafile);
-		data.setClassIndex(data.numAttributes() - 1);
- 
-		Instances testData = new Instances(dataTestfile);
-		testData.setClassIndex(testData.numAttributes() - 1);
-
-		
-		// Do 10-split cross validation
-		Instances[][] split = crossValidationSplit(data, 10);
- 
-		// Separate split into training and testing arrays
-		Instances[] trainingSplits = split[0];
-		Instances[] testingSplits = split[1];
- 
 		// Use a set of classifiers
 		Classifier[] models = { 
 				new J48(), // a decision tree
@@ -110,16 +86,22 @@ public class WekaTest {
 			// Collect every group of predictions for current model in a FastVector
 			ArrayList<Prediction> predictions = new ArrayList<Prediction>();
  
-			// For each training-testing split pair, train and test the classifier
-			for (int i = 0; i < trainingSplits.length; i++) {
-				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
+			// train and test the classifier
+			
+			Evaluation validation = classify(models[j], trainData, testData);
  
-				predictions.addAll(validation.predictions());
+			predictions.addAll(validation.predictions());
  
-				// Uncomment to see the summary for each training-testing pair.
-//				System.out.println(models[j].toString());
-			}
- 
+			// Uncomment to see the summary for each training-testing pair.
+			System.out.println("\nmodel.toStr");
+			System.out.println(models[j].toString());
+			
+			System.out.println("\ntoclassDetailStr");
+			System.out.println(validation.toClassDetailsString());
+			
+			System.out.println("\ntoMatrixStr");
+			System.out.println(validation.toMatrixString());
+								
 			// Calculate overall accuracy of current classifier on all splits
 			double accuracy = calculateAccuracy(predictions);
  
@@ -127,24 +109,160 @@ public class WekaTest {
 			// but nice-looking way.
 			System.out.println("Accuracy of " + models[j].getClass().getSimpleName() + ": "
 					+ String.format("%.2f%%", accuracy)
-					+ "\n---------------------------------");			
-		}
+					+ "\n-------------------------------------------------------------");			
+		}			
+		/*with separate train(110) and test(34) set and withoutCrossValidation
+		Accuracy of J48: 82.35%
+		---------------------------------
+		Accuracy of PART: 82.35%
+		---------------------------------
+		Accuracy of DecisionTable: 73.53%
+		---------------------------------
+		Accuracy of DecisionStump: 94.12%
+		---------------------------------
 		
-		ArrayList<Prediction> testPpred = new ArrayList<Prediction>();
+		sample Confusion Matrix for J48 and PART
+		=== Confusion Matrix ===
 		
+		  a  b   <-- classified as
+		  0  6 |  a = roleState
+		  0 28 |  b = not_roleState
+		  
+		 sample Confusion Matrix for Decision Table		 
+		=== Confusion Matrix ===
+		
+		  a  b   <-- classified as
+		  5  1 |  a = roleState
+		  8 20 |  b = not_roleState
+		  
+		  sample Confusion Matrix for Decision Stump		  
+		=== Confusion Matrix ===
+		
+		  a  b   <-- classified as
+		  4  2 |  a = roleState
+		  0 28 |  b = not_roleState
+		 */
+	}
+	
+	public static void withCrossValidation(Instances trainData, Instances testData) throws Exception {
+		
+		
+		// Do 10-split cross validation
+		Instances[][] split = crossValidationSplit(trainData, 10);
+	
+		// Separate split into training and testing arrays
+		Instances[] trainingSplits = split[0];
+		Instances[] testingSplits = split[1];
+	
+		// Use a set of classifiers
+		Classifier[] models = { 
+				new J48(), // a decision tree
+				new PART(), 
+				new DecisionTable(),//decision table majority classifier
+				new DecisionStump() //one-level decision tree
+		};
+	
+		// Run for each model
 		for (int j = 0; j < models.length; j++) {
-						
-			Evaluation testEval = classify(models[j], data, testData);
-			
-			testPpred.addAll(testEval.predictions());
-			
-			System.out.println(testEval.predictions() + "\n");
-			
-			System.out.println("Test accu " + models[j].getClass().getSimpleName() + ": "
-					+ String.format("%.2f%%", calculateAccuracy(testPpred))
-					+ "\n---------------------------------");
-		}
- 
+	
+			// Collect every group of predictions for current model in a FastVector
+			ArrayList<Prediction> predictions = new ArrayList<Prediction>();
+	
+			// For each training-testing split pair, train and test the classifier
+			for (int i = 0; i < trainingSplits.length; i++) {
+				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
+	
+				predictions.addAll(validation.predictions());
+	
+				// Uncomment to see the summary for each training-testing pair.
+				System.out.print("\n" + i + ": model.toStr ");
+				System.out.println(models[j].toString());
+				
+				System.out.println("\ntoclassDetailStr");
+				System.out.println(validation.toClassDetailsString());
 		
+				System.out.println("\ntoMatrixStr");
+				System.out.println(validation.toMatrixString());
+								
+			}
+			System.out.println("\nmodel.toStr after 10");
+			System.out.println(models[j].toString());
+			
+			// Calculate overall accuracy of current classifier on all splits
+			double accuracy = calculateAccuracy(predictions);
+	
+			// Print current classifier's name and accuracy in a complicated,
+			// but nice-looking way.
+			System.out.println("Accuracy of " + models[j].getClass().getSimpleName() + ": "
+					+ String.format("%.2f%%", accuracy)
+					+ "\n---------------------------------");	
+		}
+		/*
+		for(Instance inst:data){
+			
+			double classified = models[3].classifyInstance(inst);
+			
+			if(classified != 1.0){
+				System.out.println("data.classValue :   ****** " + inst.classValue());
+				System.out.println("data == roleState : *************** " + inst);
+				System.out.println("");
+			}
+		}*/		
+		
+		/* with separate train(110) and test(34) set (test set not used) and crossValidation
+		 Accuracy of J48: 94.55%
+		---------------------------------
+		Accuracy of PART: 94.55%
+		---------------------------------
+		Accuracy of DecisionTable: 92.73%
+		---------------------------------
+		Accuracy of DecisionStump: 92.73%
+		---------------------------------
+		
+		sample confusion matrix for J48 and PART and Decision table and Decision Stump
+		=== Confusion Matrix ===
+
+		 a  b   <-- classified as
+		 0  1 |  a = roleState
+		 0 10 |  b = not_roleState
+		  
+		 one case in each of Decision table and Decision Stump
+		 === Confusion Matrix ===
+		
+		 a b   <-- classified as
+		 0 0 | a = roleState
+		 2 9 | b = not_roleState
+
+		 */
+}
+
+	public static void main(String[] args) throws Exception {		
+		
+//		BufferedReader datafile = readDataFile("dataset/95-10-06DatasetStaticObj.arff");//stroy1
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObj.arff");//story 1&2
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObj.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetStaticObjState.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRole.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleAction.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleState-2.arff");//story1&2&3
+		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetRoleIntent-2.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObj.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObjAction.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetDynamicObjState.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetLocation.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetTime.arff");//story1&2&3
+//		BufferedReader datafile = readDataFile("dataset/95-10-13DateSetSceneGoal.arff");//story1&2&3
+		
+		Instances trainData = new Instances(datafile);
+		trainData.setClassIndex(trainData.numAttributes() - 1);
+		
+//		BufferedReader testDatafile = readDataFile("dataset/95-10-13DateSetRoleState-test.arff");//story1&2&3
+		BufferedReader testDatafile = readDataFile("dataset/95-10-13DateSetRoleIntent-test.arff");//story1&2&3
+		
+		Instances testData = new Instances(testDatafile);
+		testData.setClassIndex(testData.numAttributes() - 1);
+		
+//		withoutCrossValidation(testData, testData);
+		withCrossValidation(trainData, testData);
 	}
 }
